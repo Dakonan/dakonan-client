8c49db57988ce91f26a847d80ceccc2956c00a4b
Arrasyidff <aff.anton20@gmail.com>
2021-01-23 21:56:39 +0800

    adding stream video

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,50 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import RoomCard from '../components/RoomCard'
-import { createRoom, updateRoom } from '../redux/actions'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import RoomCard from "../components/RoomCard";
+import { createRoom, updateRoom } from "../redux/actions";
 
-import io from 'socket.io-client'
-const socket = io('http://localhost:4000')
+import io from "socket.io-client";
+const socket = io("http://localhost:4000");
 
 const Room = () => {
-    const dispatch = useDispatch()
-    const [newRoomName, setNewRoomName] = useState('')
-    const username = useSelector(state => state.players.name)
-    let rooms = useSelector(state => state.rooms.data)
-    
-    useEffect(() => {
-        dispatch(updateRoom())
-    },[dispatch])
-    
-    const handleChangeRoomName = (e) => {
-        const roomName = e.target.value
-        setNewRoomName(roomName)
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const payload = {
-            roomName: newRoomName,
-            admin: username
-        }
-        dispatch(createRoom(payload))
-    }
+  const dispatch = useDispatch();
+  const [newRoomName, setNewRoomName] = useState("");
+  const username = useSelector((state) => state.players.name);
+  let rooms = useSelector((state) => state.rooms.data);
 
-    return (
-        <div className="container">
-            <h4>{`hi ${username}`}</h4>
-            <div className="container d-flex justify-content-center">
-                <form onSubmit={handleSubmit}>
-                    <input onChange={handleChangeRoomName} type="text" />
-                    <button type="submit" className="btn-primary">Create Room</button>
-                </form>
-            </div>
-            <div className="container room-body">
-                    <h1>Room List</h1>
-                <div className="row d-flex align-items-center shadow-lg mt-5 mx-auto">
-                    <RoomCard username={username} rooms={rooms}></RoomCard>
-                </div>
-            </div>
+  useEffect(() => {
+    dispatch(updateRoom());
+  }, [dispatch]);
+
+  const handleChangeRoomName = (e) => {
+    const roomName = e.target.value;
+    setNewRoomName(roomName);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      roomName: newRoomName,
+      admin: username,
+    };
+    dispatch(createRoom(payload));
+  };
+
+  return (
+    <div className="container">
+      <h4>{`hi ${username}`}</h4>
+      <div className="container d-flex justify-content-center">
+        <form onSubmit={handleSubmit}>
+          <input onChange={handleChangeRoomName} type="text" />
+          <button type="submit" className="btn-primary">
+            Create Room
+          </button>
+        </form>
+      </div>
+      <div className="container room-body">
+        <h1>Room List</h1>
+        <div className="row d-flex align-items-center shadow-lg mt-5 mx-auto">
+          <RoomCard username={username} rooms={rooms}></RoomCard>
         </div>
-    )
-}
-export default Room
+      </div>
+    </div>
+  );
+};
+export default Room;
--- src/pages/WaitingRoom.js
+++ src/pages/WaitingRoom.js
@@ -1,47 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {Link, useParams, useHistory} from 'react-router-dom'
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams, useHistory } from "react-router-dom";
 
-import io from 'socket.io-client'
-const socket = io('http://localhost:4000')
+import io from "socket.io-client";
+const socket = io("http://localhost:4000");
 
 const WaitingRoom = () => {
-    const history = useHistory()
-    const roomDetail = useSelector(state => state.rooms.detail)
-    const username = useSelector(state => state.players.name)
-    const [startGame, setStartGame] = useState('false')
-    
-    useEffect(() => {
-        console.log('tet')
-        socket.on('start-game', (data) => {
-            console.log(data)
-            history.push(`/game/`)
-        })
-    }, [startGame])
-   
-    const start = async () => {
-        socket.emit('startGame', roomDetail)
-        setStartGame(true)
-        history.push(`/game/${roomDetail.name}`)
-    }
+  const history = useHistory();
+  const roomDetail = useSelector((state) => state.rooms.detail);
+  const username = useSelector((state) => state.players.name);
+  const [startGame, setStartGame] = useState("false");
 
-    return (
-        <div className="container">
-            <h1>lobby</h1>
-            <p>{JSON.stringify(roomDetail)}</p>
-            <div className="row">
-                <div className="col">
-                    {/* <h1>{roomDetail.users[0]}</h1> */}
-                    {
-                        username === roomDetail.admin ?
-                        <button onClick={()=> start()}>Start</button>
-                        :
-                        ""
-                    }
-                </div>
-            </div>
+  const refVideo = useRef(HTMLVideoElement, document.createElement("video"));
+  const startStreaming = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    refVideo.current.srcObject = stream;
+  };
+
+  const stopStreaming = async () => {
+    refVideo.current.srcObject = null;
+  };
+
+  useEffect(() => {
+    console.log("tet");
+    socket.on("start-game", (data) => {
+      console.log(data);
+      history.push(`/game/`);
+    });
+  }, [startGame]);
+
+  const start = async () => {
+    socket.emit("startGame", roomDetail);
+    setStartGame(true);
+    history.push(`/game/${roomDetail.name}`);
+  };
+
+  return (
+    <div className="container">
+      <h1>lobby</h1>
+      <h1>Hello World</h1>
+      <button onClick={startStreaming}>Start Streaming</button>
+      <button onClick={stopStreaming}>Stop Streaming</button>
+      <video ref={refVideo} autoPlay playsInline></video>
+      <p>{JSON.stringify(roomDetail)}</p>
+      <div className="row">
+        <div className="col">
+          {/* <h1>{roomDetail.users[0]}</h1> */}
+          {username === roomDetail.admin ? (
+            <button onClick={() => start()}>Start</button>
+          ) : (
+            ""
+          )}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default WaitingRoom
+export default WaitingRoom;
